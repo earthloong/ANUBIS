a97e7494ed8e3bd0dd47891cbc526b99df99c7df
rvisser <r.visser@chl43.nl>
2014-02-10 00:19:04 +0100

    Small message in footer regarding state of this fork

--- footer.inc.php
+++ footer.inc.php
@@ -1,5 +1,6 @@
 <?php
 
 echo "Version $version Copyright © 2012 ocminer & p_shep under <a href=\"http://www.gnu.org/copyleft/gpl.html\" target=\"_blank\">GPLv3</a>.";
+echo  "<br>You are using a development-fork made by dcm360";
 
 ?>
